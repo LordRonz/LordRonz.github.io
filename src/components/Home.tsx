705dce6d7e8340bc7cb7fd268acf06f281365d6b
LordRonz <aarontanhar2000@gmail.com>
2021-11-18 15:56:47 +0700

    fix: Uncrawlable link

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -139,7 +139,7 @@ const Home = (): JSX.Element => {
         </div>
       </Container>
       <div className="absolute bottom-0 left-1/2 text-3xl transform -translate-y-1/2 -translate-x-1/2">
-        <Link to="about" smooth>
+        <Link to="about" smooth spy hashSpy href="#about">
           <UnstyledButtonCustom ele={(
             <span>
               <Typography>
