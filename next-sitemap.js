ad462f5827a51b3fbfdae0b8cb9b51569e4a37ae
LordRonz <aarontanhar2000@gmail.com>
2021-12-10 11:09:26 +0700

    feat: add sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+// !STARTERCONF Change the siteUrl
+module.exports = {
+  siteUrl: 'https://lordronz.github.io/',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/' }],
+  },
+};
