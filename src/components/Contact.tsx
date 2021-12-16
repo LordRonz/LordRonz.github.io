c27b865dda65790f6f04cc0cf7836082f15a4125
LordRonz <aarontanhar2000@gmail.com>
2021-12-16 21:55:48 +0700

    feat: add gitlab contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,31 +8,44 @@ import React, { useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { IconContext } from 'react-icons';
 import { FiMail } from 'react-icons/fi';
-import { SiGithub, SiLinkedin } from 'react-icons/si';
+import { SiGithub, SiGitlab, SiLinkedin } from 'react-icons/si';
 
-const github = 'https://github.com/LordRonz';
 const mail = 'christopher.19072@mhs.its.ac.id';
-const linkedin = 'https://www.linkedin.com/in/aaronchristopher69/';
+
+const socials = [
+  {
+    label: 'GitHub',
+    link: 'https://github.com/LordRonz',
+    icon: <SiGithub className="text-lrtext-white transition-all duration-300 hover:text-lrred" />,
+  },
+  {
+    label: 'GitLab',
+    link: 'https://gitlab.com/lordronz',
+    icon: <SiGitlab className="text-lrtext-white transition-all duration-300 hover:text-lrred" />,
+  },
+  {
+    label: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/aaronchristopher69/',
+    icon: <SiLinkedin className="text-lrtext-white transition-all duration-300 hover:text-lrred" />,
+  },
+];
 
 const Contact = (): JSX.Element => {
   const [copyStatus, setCopyStatus] = useState('Click to copy');
 
   return (
     <Grid container direction="row" justifyContent="center" alignItems="center" spacing={3} id="contact">
-      <Grid item>
-        <Link href={github} target="_blank" rel="noopener" aria-label="Twitter">
-          <IconContext.Provider value={{ size: '2em', style: { cursor: 'pointer' } }}>
-            <SiGithub className="text-lrtext-white transition-all duration-300 hover:text-lrred" />
-          </IconContext.Provider>
-        </Link>
-      </Grid>
-      <Grid item>
-        <Link href={linkedin} target="_blank" rel="noopener" aria-label="Twitter">
-          <IconContext.Provider value={{ size: '2em', style: { cursor: 'pointer' } }}>
-            <SiLinkedin className="text-lrtext-white transition-all duration-300 hover:text-lrred" />
-          </IconContext.Provider>
-        </Link>
-      </Grid>
+      {
+        socials.map(({label, link, icon}) => (
+          <Grid item key={label}>
+            <Link href={link} target="_blank" rel="noopener" aria-label={label}>
+              <IconContext.Provider value={{ size: '2em', style: { cursor: 'pointer' } }}>
+                {icon}
+              </IconContext.Provider>
+            </Link>
+          </Grid>
+        ))
+      }
       <Grid item>
         <Tippy
           animation="scale-subtle"
