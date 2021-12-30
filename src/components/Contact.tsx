ca4148249191f060f6d1100c696bb50f1f2f3666
LordRonz <aarontanhar2000@gmail.com>
2021-12-30 10:13:45 +0700

    feat: add hackerrank contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,7 +8,7 @@ import React, { useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { IconContext } from 'react-icons';
 import { FiMail } from 'react-icons/fi';
-import { SiGithub, SiGitlab, SiLinkedin } from 'react-icons/si';
+import { SiGithub, SiGitlab, SiHackerrank, SiLinkedin } from 'react-icons/si';
 
 const mail = 'christopher.19072@mhs.its.ac.id';
 
@@ -16,17 +16,22 @@ const socials = [
   {
     label: 'GitHub',
     link: 'https://github.com/LordRonz',
-    icon: <SiGithub className='text-primary-50 transition-all duration-300 hover:text-primary-300' />,
+    icon: <SiGithub className='text-primary-50 transition-all duration-300 hover:text-primary-300' title='GitHub' />,
   },
   {
     label: 'GitLab',
     link: 'https://gitlab.com/lordronz',
-    icon: <SiGitlab className='text-primary-50 transition-all duration-300 hover:text-primary-300' />,
+    icon: <SiGitlab className='text-primary-50 transition-all duration-300 hover:text-primary-300' title='GitLab' />,
+  },
+  {
+    label: 'Hackerrank',
+    link: 'https://www.hackerrank.com/LordRonz',
+    icon: <SiHackerrank className='text-primary-50 transition-all duration-300 hover:text-primary-300' title='Hackerrank' />,
   },
   {
     label: 'LinkedIn',
     link: 'https://www.linkedin.com/in/aaronchristopher69/',
-    icon: <SiLinkedin className='text-primary-50 transition-all duration-300 hover:text-primary-300' />,
+    icon: <SiLinkedin className='text-primary-50 transition-all duration-300 hover:text-primary-300' title='LinkedIn' />,
   },
 ] as const;
 
@@ -64,7 +69,7 @@ const Contact = (): JSX.Element => {
             >
               <span>
                 <IconContext.Provider value={{ size: '2.3em', style: { cursor: 'pointer' } }}>
-                  <FiMail className='text-primary-50 transition-all duration-300 hover:text-primary-300' />
+                  <FiMail className='text-primary-50 transition-all duration-300 hover:text-primary-300' title='e-mail' />
                 </IconContext.Provider>
               </span>
             </CopyToClipboard>
