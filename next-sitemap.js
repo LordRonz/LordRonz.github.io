8cf10238ce17bb3bb23a3516a0dc84763bb93d9d
LordRonz <aarontanhar2000@gmail.com>
2022-01-25 21:47:36 +0700

    refactor: next-sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,10 @@
+/**
+ * @type {import('next-sitemap').IConfig}
+ * @see https://github.com/iamvishnusankar/next-sitemap#readme
+ */
 // !STARTERCONF Change the siteUrl
 module.exports = {
-  siteUrl: 'https://lordronz.github.io/',
+  siteUrl: 'https://lordronz.github.io',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
