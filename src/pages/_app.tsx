45cbc66c5f02642cd1a4d34853eb2bd5981bfa43
LordRonz <aarontanhar2000@gmail.com>
2022-04-10 20:00:59 +0700

    fix: add module declaration for next-themes :rocket:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,12 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 
+declare module 'next-themes' {
+  interface ThemeProviderProps {
+    children: React.ReactNode;
+  }
+}
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false}>
